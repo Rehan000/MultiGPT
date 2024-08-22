e1e019ea17e98a799a12b10bb987f08f87415845
Muhammad Rehan <mrehan246@gmail.com>
2024-08-22 20:16:19 +0500

    Initial Commit

---
+++ utils.py
@@ -0,0 +1,21 @@
+import json
+from datetime import datetime
+from langchain.schema.messages import HumanMessage, AIMessage
+
+
+def save_chat_history_json(chat_history, file_path):
+    with open(file_path, "w") as f:
+        json_data = [message.dict() for message in chat_history]
+        json.dump(json_data, f)
+
+
+def load_chat_history_json(file_path):
+    with open(file_path, "r") as f:
+        json_data = json.load(f)
+        messages = [HumanMessage(**message) if message["type"] == "human" else AIMessage(**message) for message in
+                    json_data]
+        return messages
+
+
+def get_timestamp():
+    return datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
